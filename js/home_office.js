efc859a1db7db61575d3691716ed8cff00a5ca95
Rodrigo Faria <rodrigolfsi@gmail.com>
2020-10-11 16:18:52 -0300

    Update test

--- js/home_office.js
+++ js/home_office.js
@@ -1,14 +1,28 @@
 const saveButtonEvent = () => {
     
-    let p = {
-        post: document.getElementById('data').value
-    };
+    // let p = {
+    //     post: document.getElementById('data').value
+    // };
     
-    $.post("https://news-api-node.herokuapp.com/api/v1/news/796313bd-625b-4542-ac2c-f34b43967ccc", p)
-        .done(() => {
-            console.log('sucesso cara')
-        }
-    );
+    // $.post("https://news-api-node.herokuapp.com/api/v1/news/796313bd-625b-4542-ac2c-f34b43967ccc", p)
+    //     .done(() => {
+    //         console.log('sucesso cara')
+    //     }
+    // );
+
+    var settings = {
+        "url": "https://news-api-node.herokuapp.com/api/v1/news/796313bd-625b-4542-ac2c-f34b43967ccc",
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+          "Content-Type": "application/json"
+        },
+        "data": JSON.stringify({"post":"<h1>Direto JS</h1><p>Teste 1/10</p>"}),
+      };
+      
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+      });
 }
 
 window.onload = () => {
