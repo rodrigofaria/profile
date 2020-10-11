42f0fdb280063bb176e6adf2cc074db270e1a3ed
Rodrigo Faria <rodrigolfsi@gmail.com>
2020-10-11 16:15:51 -0300

    Created page to test Selenium

--- js/home_office.js
+++ js/home_office.js
@@ -1,44 +1,16 @@
-
-calc_period();
-
-function calc_period() {
-    let counter = 0;
+const saveButtonEvent = () => {
     
-    let day = new Date(2020, 02, 20);
-    let today = getTodayDate();
-
-    do {
-        if (day.getDay() != 0 && day.getDay() != 6) {
-            counter++;
+    let p = {
+        post: document.getElementById('data').value
+    };
+    
+    $.post("https://news-api-node.herokuapp.com/api/v1/news/796313bd-625b-4542-ac2c-f34b43967ccc", p)
+        .done(() => {
+            console.log('sucesso cara')
         }
-        
-        if (day.getTime() === today.getTime()) {
-            break;
-        }
-        day.setDate(day.getDate() + 1);
-    } while(true);
-
-    printDaysCounter(counter);
-    printHoursCounter(counter * 90);
+    );
 }
 
-function printDaysCounter(counter) {
-    document.getElementById('days').innerHTML = counter;
-}
-
-function printHoursCounter(minutes) {
-    let hours = parseInt(minutes / 60);
-    minutes = minutes - (60 * hours);
-
-    document.getElementById('hours').innerHTML = hours + ' hours ' + minutes + ' minutes ';
-}
-
-function getTodayDate() {
-    let today = new Date();
-    today.setHours(0);
-    today.setMinutes(0);
-    today.setSeconds(0);
-    today.setMilliseconds(0);
-
-    return today;
+window.onload = () => {
+    document.getElementById('saveButton').onclick = saveButtonEvent
 }
